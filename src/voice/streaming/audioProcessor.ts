872828cf6d32cd259d250fd6dc6f21221798afc9
shahil-kv <mshahilkv@gmail.com>
2025-10-17 12:40:19 +0530

    the uat stage is removed only production and development

--- src/voice/streaming/audioProcessor.ts
+++ src/voice/streaming/audioProcessor.ts
@@ -1,5 +1,6 @@
 import alawmulaw from "alawmulaw";
-import { logger } from "../../utils/logger.js";
+import { logger } from "../../utils/logger";
+// import { logger } from "../../utils/logger.js";
 import libsamplerate from "@alexanderolsen/libsamplerate-js";
 const { create, ConverterType } = libsamplerate;
 
